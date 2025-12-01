b43a8e269f2d926621a7a46fc451b5cadc85de59
k-avy <kavyuushukla59@gmail.com>
2025-12-02 02:20:14 +0530

    chore: rename app to EventNotificationSystem and update build metadata

---
+++ EventNotificationSystem/src/main/java/github/com/kavy/EventNotificationSystemApplication.java
@@ -0,0 +1,18 @@
+package github.com.kavy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventNotificationSystemApplication extends Object {
+
+    public EventNotificationSystemApplication() {
+        super();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventNotificationSystemApplication.class, args);
+    }
+}
+
+
